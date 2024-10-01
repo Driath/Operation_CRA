28f4e51943875046e8dc9ee1294b195e75f7c2de
Matthieu CZESKI <matthieu.czeski@gmail.com>
2024-10-01 15:26:27 +0200

    feat: add color feature

---
+++ src/color-indicator/color-indicator.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-color-indicator',
+    standalone: true,
+    // imports: [CommonModule],
+    template: ` <div [style.backgroundColor]="color" class="w-2 h-2 rounded-full"></div> `,
+})
+export class ColorIndicatorComponent {
+    @Input({ required: true }) color!: string;
+}
