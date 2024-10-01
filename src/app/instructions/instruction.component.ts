aca56dd764aef714442edb21cbf5f44078ee4479
Matthieu CZESKI <matthieu.czeski@gmail.com>
2024-10-01 17:21:08 +0200

    fix: better insttructions

--- src/app/instructions/instruction.component.ts
+++ src/app/instructions/instruction.component.ts
@@ -7,7 +7,7 @@ import { CardModule } from 'primeng/card';
     template: `
         <p-card>
             <p>
-                Hello cher administra(teur/trice) de nos chers valeurs agents,<br />
+                Hello cher(e) administra(teur/trice) de nos chers valeureux Agents,<br />
                 Afin d'inputer, il faut selectionner un agent, une mission, et vous aurez la
                 possibilité de toggle la mission selectionnée selon l'agent selectionné en cliquant
                 sur la date de l'imputation voulue
